c970bc07d5a02e485caa05c4a45f68904b52321d
Tony <tony.js@zoho.eu>
2020-10-31 17:23:49 -0300

    test: temporary test in heroku

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
-let store
-if ( process.env.NODE_ENV === 'production' ) store = require( './index.prod' ).default
-else if ( process.env.NODE_ENV === 'test' ) store = require( './index.prod' ).default
-else store = require( './index.dev' ).default
+let store = require( './index.prod' )
+// if ( process.env.NODE_ENV === 'production' ) store = require( './index.prod' ).default
+// else if ( process.env.NODE_ENV === 'test' ) store = require( './index.prod' ).default
+// else store = require( './index.dev' ).default
 
 export default store
